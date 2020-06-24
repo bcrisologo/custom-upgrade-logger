87f1acf719f64f21eee6fac91944beded229ee6e
bc-ubuntu-desktop <jblaise0912@gmail.com>
2020-06-23 18:06:03 -0700

    Added script

---
+++ upgrade-script-dry-run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Source update
+apt-get update
+
+# Upgrading script
+date +"%n%m/%d/%y %H:%M:%S:%nThe following packages need to be UPGRADED:" >> /var/log/upgrades-script.log && apt-get -s upgrade | awk 'BEGIN{counter=0} /^The following packages will be upgraded:$/ {counter++; next;} /^[0-9]+ upgraded/ {exit;} counter>0 {gsub(/^ +/, "", $0); print;}' >> /var/log/upgrades-script.log
+
+# Autoremove script
+echo -e "\nThe following needs to be REMOVED:" >> /var/log/upgrades-script.log && apt-get -s autoremove | awk 'BEGIN{counter=0} /^The following packages will be REMOVED:$/ {counter++; next;} /^[0-9]+ upgraded/ {exit;} counter>0 {gsub(/^ +/, "", $0); print;}' >> /var/log/upgrades-script.log
